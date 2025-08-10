5dce79e6e653608a43b98ed9d34b7b57ae12661c
Jaron <Jaronlendering@gmail.com>
2025-08-10 21:36:09 +0200

    changed comment hello_world

--- BareMinimum/UART_helloWorld/src/hello_world.c
+++ BareMinimum/UART_helloWorld/src/hello_world.c
@@ -4,7 +4,7 @@
 
 void main(void) {
   UART_println("hello world");
-  while(1){ //To make the program go on for ever, such that the pie doesn't crash because there are no instructions anymore
+  while(1){ //To make the program go on for ever
         __asm__ volatile("nop"); // makes sure that the empty while will not be optimized away.
   }
 }
